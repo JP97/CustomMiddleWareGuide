ebf36a0f78781ac002854c761089284389901163
Julian <juli11c0@edu.eal.dk>
2019-10-03 10:44:07 +0200

    Add project files.

---
+++ CustomMiddleWareGuide/HelloMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWareGuide
+{
+    public class HelloWorldMiddleware
+    {
+        private readonly RequestDelegate next;
+        public HelloWorldMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World! (Use in Class)\n");
+            await this.next(context);
+        }
+    }
+}
---
+++ CustomMiddleWareGuide/MiddlewareExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWareGuide
+{
+    public static class MiddlewareExtension
+    {
+        public static void RunHelloWorld(this IApplicationBuilder app)
+        {
+            app.Run(async (Context) =>
+            {
+                await Context.Response.WriteAsync("Hello World!(Run)");
+                
+            });
+        }
+
+        public static IApplicationBuilder UseHelloWorld(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloWorldMiddleware>();
+        }
+
+        public static IApplicationBuilder UseHelloWorldInClass(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloWorldMiddleware>();
+        }
+    } 
+}
